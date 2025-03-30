f5318e5faeb04cdea9da7cdacd9737bf0ced3d6a
debbbl <dtshiting@gmail.com>
2025-03-30 21:59:15 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/foodx/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
